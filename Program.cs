270e056815090cadd9d0afeacec40a3911cd610b
sangmoki <yoochang4176@naver.com>
2024-07-09 14:16:11 +0900

    Feat: "별찍기 로직 구현"

--- Program.cs
+++ Program.cs
@@ -7,23 +7,23 @@ namespace MultipulicationTables
         static void Main(string[] args)
         {
             // 구구단 계산
-            for (int i = 2; i <= 9; i++)
-            {
-                for (int j = 1; j <= 9; j++)
-                {
-                    Console.WriteLine($"{i} * {j} = {i * j}");
-                }
-            }
+            /*            for (int i = 2; i <= 9; i++)
+                        {
+                            for (int j = 1; j <= 9; j++)
+                            {
+                                Console.WriteLine($"{i} * {j} = {i * j}");
+                            }
+                        }*/
 
             // 별찍기 1-5까지
-            /*            for (int i = 0; i < 5; i++)
-                        {
-                            for (int j = 0; j <= i; j++)
-                            {
-                                Console.WriteLine("*");
-                            }
-                            Console.WriteLine();
-                        }*/
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
