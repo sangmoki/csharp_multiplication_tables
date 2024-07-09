f3d61bd2320856ee054aab36bb714f3dcfca9a64
sangmoki <yoochang4176@naver.com>
2024-07-09 14:24:19 +0900

    Feat: "팩토리얼 계산 로직 완료"

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ namespace MultipulicationTables
 {
     class Program
     {
+        // Factorial 이란 n값을 -1 해가며 곱 계산
+        static int Factorial(int n)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
             // 구구단 계산
@@ -16,14 +29,18 @@ namespace MultipulicationTables
                         }*/
 
             // 별찍기 1-5까지
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.WriteLine("*");
-                }
-                Console.WriteLine();
-            }
+            /*            for (int i = 0; i < 5; i++)
+                        {
+                            for (int j = 0; j <= i; j++)
+                            {
+                                Console.WriteLine("*");
+                            }
+                            Console.WriteLine();
+                        }*/
+
+            // 팩토리얼 계산 5에서 1까지 -1씩 뺀 숫자 곱하기
+            int ret = Factorial(5);
+            Console.WriteLine(ret);
         }
     }
 }
