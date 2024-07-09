04146e456453c198b8a60f27bf2d5e0cac31d3c3
sangmoki <yoochang4176@naver.com>
2024-07-09 14:15:55 +0900

    Feat: "구구단 계산 로직 구현 1단부터 9단까지"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MultipulicationTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 구구단 계산
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+
+            // 별찍기 1-5까지
+            /*            for (int i = 0; i < 5; i++)
+                        {
+                            for (int j = 0; j <= i; j++)
+                            {
+                                Console.WriteLine("*");
+                            }
+                            Console.WriteLine();
+                        }*/
+        }
+    }
+}
